11fd357da7d901301a357af6a65b20d9f4eddcb8
Don Gochenour <don@rti.com>
2018-06-29 11:55:39 -0400

    add lab #6

---
+++ 06/quickstartSupport.h
@@ -0,0 +1,55 @@
+
+/*
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+This file was generated from quickstart.idl using "rtiddsgen".
+The rtiddsgen tool is part of the RTI Connext distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the RTI Connext manual.
+*/
+
+#ifndef quickstartSupport_214727369_h
+#define quickstartSupport_214727369_h
+
+/* Uses */
+#include "quickstart.h"
+
+#ifndef ndds_c_h
+#include "ndds/ndds_c.h"
+#endif
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+
+#endif
+
+/* ========================================================================= */
+/**
+Uses:     T
+
+Defines:  TTypeSupport, TDataWriter, TDataReader
+
+Organized using the well-documented "Generics Pattern" for
+implementing generics in C and C++.
+*/
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, start exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport __declspec(dllexport)
+
+#endif
+
+DDS_TYPESUPPORT_C(qs_qs_typeTypeSupport, qs_qs_type);
+DDS_DATAWRITER_C(qs_qs_typeDataWriter, qs_qs_type);
+DDS_DATAREADER_C(qs_qs_typeDataReader, qs_qs_typeSeq, qs_qs_type);
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, stop exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport
+#endif
+
+#endif  /* quickstartSupport_214727369_h */
+
